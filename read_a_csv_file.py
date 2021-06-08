540d1a9e5b577f46297152c1c25dab4f771f04d5
Sk Nasimul Alahi <59950232+snalahi@users.noreply.github.com>
2021-06-08 00:53:48 -0700

    Week 1

---
+++ read_a_csv_file.py
@@ -0,0 +1,14 @@
+ fileconnection = open("olympics.txt", 'r')
+ lines = fileconnection.readlines()
+ header = lines[0]
+ field_names = header.strip().split(',')
+ print(field_names)
+ for row in lines[1:]:
+     # .split(',') => splits the line by comma
+     # .strip() => strips out all the new line characters 
+     vals = row.strip().split(',')
+     if vals[5] != "NA":
+         print("{}: {}; {}".format(
+                 vals[0],
+                 vals[4],
+                 vals[5]))
